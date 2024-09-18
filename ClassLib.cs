e29f682cb41e36370e74941ea20753588e4e8fdb
liza <liza@DESKTOP-OST2SGM>
2024-09-18 11:43:12 +0300

    Add project files.

---
+++ ClassLib.cs
@@ -0,0 +1,33 @@
+﻿namespace Lab2lib
+{
+	
+		public class Book
+		{
+			public string Title;
+			public string Author;
+			public int NumPages;
+			public int WordCount;
+			public string Publisher;
+			public int YearPublished;
+			public string Language;
+			public string Genre;
+
+
+			public double CalculateWordsPerPage()
+			{
+				if (NumPages > 0)
+				{
+					return (double)WordCount / NumPages;
+				}
+				else
+				{
+					Console.WriteLine("Кількість сторінок не може бути 0 або менше.");
+					return 0;
+				}
+			}
+			public bool IsLargeBook()
+			{
+				return NumPages >= 500;
+			}
+		}
+}
